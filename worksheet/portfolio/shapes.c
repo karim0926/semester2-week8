4f2dbbbb28cde8f87a0f25a3ac2ae80faeed3bf9
karim0926 <rzvh0240@leeds.ac.uk>
2026-05-01 12:51:01 +0000

    run

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -14,3 +14,70 @@ Point makePoint( float x, float y ) {
 // - start with stubs as above
 // - compile regularly to test syntax
 // - test functions by calling them from main()
+Point makePoint(float x, float y) {
+    Point new;
+
+    new.x = x;
+    new.y = y;
+
+    return new;
+}
+
+Line makeLine(Point p1, Point p2) {
+    Line new;
+
+    new.p[0] = p1;
+    new.p[1] = p2;
+
+    return new;
+}
+
+Triangle makeTriangle(Point p1, Point p2, Point p3) {
+    Triangle new;
+
+    new.p[0] = p1;
+    new.p[1] = p2;
+    new.p[2] = p3;
+
+    return new;
+}
+
+float lineLength(Line l) {
+    float dx = l.p[1].x - l.p[0].x;
+    float dy = l.p[1].y - l.p[0].y;
+
+    return sqrtf((dx * dx) + (dy * dy));
+}
+
+float triangleArea(Triangle t) {
+    float x1 = t.p[0].x;
+    float y1 = t.p[0].y;
+
+    float x2 = t.p[1].x;
+    float y2 = t.p[1].y;
+
+    float x3 = t.p[2].x;
+    float y3 = t.p[2].y;
+
+    return fabsf((x1 * (y2 - y3)) +
+                 (x2 * (y3 - y1)) +
+                 (x3 * (y1 - y2))) / 2.0f;
+}
+
+bool samePoint(Point p1, Point p2) {
+    float tolerance = 1.0e-6f;
+
+    return fabsf(p1.x - p2.x) < tolerance &&
+           fabsf(p1.y - p2.y) < tolerance;
+}
+
+bool pointInLine(Point p, Line l) {
+    return samePoint(p, l.p[0]) ||
+           samePoint(p, l.p[1]);
+}
+
+bool pointInTriangle(Point p, Triangle t) {
+    return samePoint(p, t.p[0]) ||
+           samePoint(p, t.p[1]) ||
+           samePoint(p, t.p[2]);
+}
