e8ffb591fa4b6ac5ce2dbda34bcbde3247592e8b
karim0926 <rzvh0240@leeds.ac.uk>
2026-05-01 13:57:11 +0100

    Refactor makePoint function implementation
    Refactored makePoint function to initialize Point structure.

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -1,19 +1,7 @@
-
 #include <stdbool.h>
 #include <math.h>
 
 #include "shapes.h"
-
-Point makePoint( float x, float y ) {
-    Point new;
-    // implementation
-    return new;
-}
-
-// complete other functions below
-// - start with stubs as above
-// - compile regularly to test syntax
-// - test functions by calling them from main()
 Point makePoint(float x, float y) {
     Point new;
 
